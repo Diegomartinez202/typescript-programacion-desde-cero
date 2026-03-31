a5fea5bbb26650632ded521ade6df22b68e0453b
Diegomartinez202 <diegomartin0102@gmail.com>
2026-03-30 19:34:23 -0500

    Priemr commit programacion desde cero

---
+++ app.ts
@@ -0,0 +1,27 @@
+console.log("Hello, Typescript");
+
+type User = {
+    id: number;
+    name: string;
+    email: string;
+};
+
+const user: User = {
+    id: 1,
+    name: "John Doe",
+    email: "jhon.@example.com"
+};
+const user2: User = {
+    id: 2,
+    name: "Jane Doe",
+    email: "jane.@example.com"
+};
+console.log(user);
+
+function getUser(id: number): User | undefined {
+    if (id === user.id){
+        return user;
+    }
+   return undefined;
+    }
+
